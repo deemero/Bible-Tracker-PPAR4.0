17c1be918b1c36141b41c150d7578c3b1a2f74bc
deemero <neroalex93@gmail.com>
2025-03-16 11:10:30 +0800

    test

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@
 import { useEffect, useState } from 'react';
 // import { supabase } from '@/lib/supabaseClient';
 import { supabase } from '@/lib/supabaseClient';
-
-// import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 
 
