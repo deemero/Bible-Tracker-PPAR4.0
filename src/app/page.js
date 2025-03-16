c97c96f9aa688fadd8c0a6e423510361472247c9
deemero <neroalex93@gmail.com>
2025-03-16 11:01:32 +0800

    fix import path

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 "use client";  
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+// import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
+
 
 
 export default function Home() {
