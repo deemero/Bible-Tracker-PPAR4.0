4a0a59acf673b11f5839056e43e8ab57183038ea
deemero <neroalex93@gmail.com>
2025-03-30 01:05:03 +0800

    noficaiton

---
+++ pages/api/_app.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { useUser } from '@supabase/auth-helpers-react' // or your own Supabase auth hook
+
+export default function MyApp({ Component, pageProps }) {
+  const user = useUser();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window?.OneSignal) {
+      window.OneSignal = window.OneSignal || [];
+      window.OneSignal.push(function () {
+        window.OneSignal.init({
+          appId: 'e730a55c-a4a5-4a74-b15b-984a234f0010',
+          notifyButton: {
+            enable: true,
+          },
+        });
+
+        // Assign user ID if login
+        if (user?.id) {
+          window.OneSignal.setExternalUserId(user.id);
+        } else {
+          window.OneSignal.removeExternalUserId();
+        }
+      });
+    }
+  }, [user]);
+
+  return <Component {...pageProps} />
+}
