3c6ba2cb1c5e3409d38d30031720ae29f513796b
deemero <neroalex93@gmail.com>
2025-04-01 15:37:20 +0800

    fix icon apple

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@
 import "../globals.css";
 import ClientLayout from "@/components/ClientLayout";
 
-
 export const metadata = {
   title: "Bible Tracker",
   description: "Reading App",
@@ -11,13 +10,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Manifest & PWA Settings */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+
+        {/* iOS Specific */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/bpr-180.png" />
+      </head>
       <body className="bg-background text-foreground">
-      <ClientLayout>{children}</ClientLayout>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
 }
-
 
 
 
