46fec1ef96014eb2e1659dd63856e173617404b5
deemero <neroalex93@gmail.com>
2025-04-16 17:41:37 +0800

    gas

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,10 +4,14 @@ import ClientLayout from "@/components/ClientLayout";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "react-hot-toast";
 import AutoLogoutProvider from "@/components/AutoLogoutProvider";
+import { icons } from "lucide-react";
 
 export const metadata = {
   title: "Bible Tracker",
   description: "Reading App",
+  icons: {
+    icon: '/bpre.png',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -18,7 +22,7 @@ export default function RootLayout({ children }) {
         <meta name="theme-color" content="#0f172a" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/bpr-180.png" />
+        <link rel="bprp.png" sizes="180x180" href="/bpr-180.png" />
       </head>
       <body className="bg-white text-gray-900 h-full"> {/* ✅ Back to white/light for Global */}
         <AutoLogoutProvider>
